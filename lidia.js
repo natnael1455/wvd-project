3163b739664d47bc6a11b3da03849a05c3fbc75f
lidiatekeste2312 <lidiatekeste2312@users.noreply.github.com>
2021-02-13 22:12:55 +0200

    tree with option for autumn leaf color

--- lidia.js
+++ lidia.js
@@ -8,16 +8,18 @@ function display(){
 
 	canvas.width=SIZE;
 	canvas.height=SIZE;
-	drawTree(ctx,[SIZE/2,SIZE-50],'green', 200, 0, 30);
+	
+	drawTree(ctx,'autumn',[SIZE/2,SIZE-50],'green', 200, 0, 30);
 	}
 
 
-function drawTree(ctx,loc,color,height, angle, thick){
+function drawTree(ctx,season,loc,color,height, angle, thick){
 	
 	ctx.beginPath();
 	ctx.save();
-	ctx.strokeStyle = 'brown';
-	ctx.fillStyle = color;
+	ctx.strokeStyle = 'rgb(145,94,47)';
+    ctx.shadowBlur = 20;
+    ctx.shadowColor = 'rgb(145,94,47)';
 	ctx.lineWidth = thick;
 	ctx.translate(loc[0],loc[1]);
 	ctx.rotate(angle*Math.PI/180);
@@ -26,9 +28,21 @@ function drawTree(ctx,loc,color,height, angle, thick){
 	ctx.stroke();
 	//console.log(height)
 	
-	if (height<10){
+	if (height<5){
 		ctx.beginPath();
-		ctx.arc(0,-length,10,0,Math.PI/2);
+		if (season==='autumn'){
+			var grd = ctx.createLinearGradient(0, -height, -height+5, -height+5);
+			grd.addColorStop(0, "red");
+			grd.addColorStop(0.5, "orange");
+			ctx.fillStyle = grd;
+			ctx.shadowColor = 'brown';
+		}
+		else{
+			ctx.fillStyle = color;
+			ctx.shadowColor = color;
+		}
+        ctx.shadowBlur = 20;
+		ctx.arc(0,-height,15,0,Math.PI/2.5);
 		ctx.fill();
 		ctx.restore();
 		
@@ -36,9 +50,9 @@ function drawTree(ctx,loc,color,height, angle, thick){
 	}
 	
 	
-	drawTree(ctx,[0,-height],'green',height*0.75, angle+8, thick*0.6);
-
-	drawTree(ctx,[0,-height],'green',height*0.75,angle-8, thick*0.6);
+	drawTree(ctx,season,[0,-height],color,height*0.75, angle+10, thick*0.6);
+	drawTree(ctx,season,[0,-height],color,height*0.75,angle-10, thick*0.6);
+	
 	
 	ctx.restore();
 	
